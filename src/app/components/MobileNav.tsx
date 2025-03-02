7356077d0b8a53b9d2305629b87b33acf4c3de05
Zaid Saifur Rahman <zaidrahman342@gmail.com>
2025-03-02 13:44:51 +0530

    Nav Bar Added

--- src/app/components/MobileNav.tsx
+++ src/app/components/MobileNav.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
 import { motion } from "framer-motion";
+import Navigationbar from "./Navigationbar";
 
-const HamIcon = () => {
+const HamIcon = ({ opennav }) => {
   return (
     <motion.svg
       xmlns="http://www.w3.org/2000/svg"
@@ -12,18 +14,45 @@ const HamIcon = () => {
       className="cursor-pointer"
       animate={{ scale: [0.8, 1.1, 0.8] }} // Enlarges and shrinks
       transition={{ repeat: Infinity, duration: 1.2, ease: "easeInOut" }} // Smooth looping
+      onClick={opennav} // Pass function to update state
     >
       <path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM64 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L96 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z" />
     </motion.svg>
   );
 };
 
-const MobileNav = () => {
+const CloseIcon = ({closenav}) => {
   return (
-    <nav className="w-full h-full px-4 lg:hidden flex items-center justify-between">
-      <Image src="/Fb_logo.jpg" width={60} height={60} alt="logo" />
-      <HamIcon />
-    </nav>
+    <motion.svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 384 512"
+      width={30}
+      fill="#130535ca"
+      onClick={closenav}
+      className="cursor-pointer"
+      animate={{ scale: [0.8, 1.1, 0.8] }} // Enlarges and shrinks
+      transition={{ repeat: Infinity, duration: 1.2, ease: "easeInOut" }} // Smooth looping
+    >
+      <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+    </motion.svg>
+  );
+};
+
+const MobileNav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <nav className="w-full h-full px-4 lg:hidden flex items-center justify-between">
+        <Image src="/Fb_logo.jpg" width={60} height={60} alt="logo" />
+        {isOpen ? (
+          <CloseIcon closenav={() => setIsOpen(false)} />
+        ) : (
+          <HamIcon opennav={() => setIsOpen(true)} />
+        )}
+      </nav>
+      <Navigationbar isOpen={isOpen} />
+    </>
   );
 };
 
---
+++ src/app/components/Navigationbar.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { motion } from "framer-motion";
+// import axios from "axios";
+
+const Navigationbar = ({ isOpen }) => {
+  return (
+    <motion.nav
+      initial={{ y: "100%" }} // Initially off-screen
+      animate={{ y: isOpen ? "0%" : "100%" }} // Show/hide based on `isOpen`
+      transition={{ duration: 0.5, ease: "easeInOut" }}
+      className="w-[100vw] fixed bottom-0 h-[92vh] bg-[#FF5D00] shadow-2xl"
+    >
+      
+    </motion.nav>
+  );
+};
+
+export default Navigationbar;
